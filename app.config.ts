743cd76619a84c09700138877e25911f1200b89c
ryoAccount <jane.doe3wtp4649@gmail.com>
2023-09-19 06:06:44 +0900

    initial commit

---
+++ app.config.ts
@@ -0,0 +1,13 @@
+export default defineAppConfig({
+  // Configure Content Wind here
+  cover: '/ogp.png',
+  socials: {
+    twitter: 'RYO_nami',
+    github: 'ryoAccount'
+  },
+  prose: {
+    h1: {
+      icon: 'carbon:sailboat-coastal'
+    }
+  }
+})
